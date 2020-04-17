4c57599c3b6f3e18cc82e879e979e2d773e38483
David Chung <dbchung3@yahoo.com>
2020-04-17 00:50:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,9 @@
-require 'pry'
 
-def reverse_each_word(pass)
-  new_array = []
-  new_array<< pass.join(" ")
-  new_array
- 
-binding.pry
+
+def reverse_each_word(argument)
+  
+ argument
+
   
 end
+
