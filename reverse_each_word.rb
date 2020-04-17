dc7bf8c3cda12eaf3761b3722e1a7f672708e3ba
David Chung <dbchung3@yahoo.com>
2020-04-17 01:03:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,20 +1,10 @@
-
-
-def reverse_each_word(argument)
-  new_array = []
- new_array << argument.split
- new_array.each {|n| n.reverse}
- 
-end
-
-
 def reverse_each_word(string)
   array1 = string.split(" ")
   new_array = [] 
   array1.each {|n|
   new_array << n.reverse
   }
-  array1.join(" ")
+  new_array.join(" ")
 end 
 
   
